cf3b25d7c117641549a5c786741402eddb382f0e
Grant Moore <chinchalinchin@gmail.com>
2023-07-28 13:28:30 -0400

    add conditional module for kms key, add dead letter queue, logging, tracing, etc

--- local.tf
+++ local.tf
@@ -1,8 +1,25 @@
 locals {
-    vpc_map                     = try({
-        vpc_config              = var.lambda.vpc_config
-    }, {})
-    env_map                     = try({
-        environment_variables   = var.lambda.environment_variables
-    }, {})
+    # Constants
+    event_notification_id           = "${var.lambda.function_name}-notifications"
+    event_notification_arn          = "arn:aws:sns:*:*:${local.event_notification_id}"
+    # Calculations
+    conditions                      = {
+        provision_key               = var.lambda.key == null
+    }
+    # Configurations
+    encryption_configuration        = local.conditions.provision_key ? (
+                                        module.kms[0].key 
+                                    ) : (
+                                        var.bucket.key
+                                    )
+    environment_configuration       = length(var.lambda.environment_variables) > 0 ? (
+                                        toset(["placeholder"])
+                                    ) : ( 
+                                        toset([])
+                                    )
+    vpc_configuration               = length(var.lambda.vpc_config) > 0 ? (
+                                        toset(["placeholder"])
+                                    ) : ( 
+                                        toset([])
+                                    )
 }
--- resource.tf
+++ resource.tf
@@ -1,30 +1,62 @@
 resource "aws_lambda_function" "this" {
-    function_name               = var.lambda.function_name
-    image_uri                   = var.lambda.image_url
-    kms_key_arn                 = var.lambda.kms_key_arn
-    memory_size                 = var.lambda.memory
-    package_type                = "Image"
-    publish                     = true
-    role_arn                    = var.lambda.execution_role_arn
-    timeout                     = var.lambda.timeout
+    #checkov:skip=CKV_AWS_272: "Ensure AWS Lambda function is configured to validate code-signing"
+        # TODO: sign ECR images
+
+    function_name                   = var.lambda.function_name
+    image_uri                       = var.lambda.image_url
+    kms_key_arn                     = local.encryption_configuration.arn
+    memory_size                     = var.lambda.memory
+    package_type                    = "Image"
+    publish                         = true
+    role_arn                        = var.lambda.execution_role.arn
+    timeout                         = var.lambda.timeout
+    reserved_concurrent_executions  = var.lambda.reserved_concurrent_executions
+    
+    dead_letter_config {
+        target_arn                  = aws_sns_topic.this.arn
+    }
+
+    tracing_config {
+        mode                        = var.lambda.tracing_config_mode
+    }
 
     dynamic "environment" {
-        for_each                = local.env_map
+        for_each                    = local.environment_configuration
 
         content {
-            variables           = each.value.environment_variables
+            variables               = var.lambda.environment_variables
         }
     }
 
     dynamic "vpc_config" {
-        for_each                = local.vpc_map
+        for_each                    = local.vpc_configuration
 
         content {
-            subnet_ids          = each.value.vpc_config.subnet_ids
-            security_group_ids  = each.value.vpc_config.security_group_ids
+            subnet_ids              = var.lambda.vpc_config.subnet_ids
+            security_group_ids      = var.lambda.vpc_config.security_group_ids
         }
     }
-    environment {
-        variables       = var.lambda.environment_variables
-    }
+}
+
+resource "aws_cloudwatch_log_group" "this" {
+  name                              = "/aws/lambda/${var.lambda.function_name}"
+  retention_in_days                 = 14
+}
+
+
+resource "aws_sns_topic" "this" {
+    kms_master_key_id               = local.encryption_configuration.alias_arn
+    name                            = local.event_notification_id
+    policy                          = data.aws_iam_policy_document.notification_resource_policy.json
+}
+
+resource "aws_iam_policy" "this" {
+  name                              = "${var.lambda.function_name}-sns-dlq-policy"
+  description                       = "Allows publishing to the SNS Topic '${local.event_notification_id}'"
+  policy                            = data.aws_iam_policy_document.policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "this" {
+  role                              = var.lambda.execution_role.name
+  policy_arn                        = aws_iam_policy.this.arn
 }
