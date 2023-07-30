1b1edf1b80146c259159c2e63ab909b3e6b0c7a4
cumberland-cloud <the.cumberland.cloud@gmail.com>
2023-07-30 13:46:51 -0400

    address as git source

--- module.tf
+++ module.tf
@@ -1,6 +1,6 @@
 module "kms" {
     count           = local.conditions.provision_key ? 1 : 0
-    source          = "https://github.com/cumberland-cloud/modules-kms.git?ref=v1.0.0"
+    source          = "git::https://github.com/cumberland-cloud/modules-kms.git?ref=v1.0.0"
 
     key             = {
         alias       = "${local.name}-s3"
