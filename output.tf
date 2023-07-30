89fe32b91b9d28bd18760f41aec6aab222240f95
cumberland-cloud <the.cumberland.cloud@gmail.com>
2023-07-30 13:45:15 -0400

    add function name to output, use version tag for module source

--- output.tf
+++ output.tf
@@ -2,6 +2,6 @@ output "function" {
     value           = {
         arn         = aws_lambda_function.this.arn
         invoke_arn  = aws_lambda_function.this.invoke_arn
-        name        = "TODO"
+        name        = var.lambda.function_name
     }
 }
